d4d7fcb0043e4bee74a782a698c566564efdf813
HenriquePoltronieri <henrique.poltronieri2008@gmail.com>
2026-07-01 18:27:52 -0300

    simplificação do código

--- backend/services/get_activity_service.py
+++
@@ -1,9 +0,0 @@
-from repositories.activity_repository import ActivityRepository
-
-
-class GetActivityService:
-    def __init__(self):
-        self.repository = ActivityRepository()
-
-    def execute(self, activity_id):
-        return self.repository.find_by_id(activity_id)
